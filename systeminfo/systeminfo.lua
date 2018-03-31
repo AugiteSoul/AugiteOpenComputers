ef19efaca914df56e665cecba548d9b2f06aa41c
AugiteSoul <arthursibra@gmail.com>
2018-03-31 14:42:48 +0200

    optimized the whole thing

--- systeminfo/systeminfo.lua
+++ systeminfo/systeminfo.lua
@@ -5,7 +5,7 @@ local component = require("component")
 local gpu = component.gpu
 local term = require("term")
 
---internet part, thanks to payonel@irc
+--internet stuff, thanks to payonel@irc
 local internet
 if component.isAvailable("internet")
 then 
@@ -17,7 +17,8 @@ gpu.setForeground(0xC14892)
 term.write("Systeminfo by AugiteSoul/augitesoul@twitter/augitesoul.fr/augitesoul@ocforums\n\n")
 
 os.sleep(0.25)
---internet part, thanks to CptMercury@ocforums - disabled for now
+--internet part, thanks to CptMercury@ocforums
+if component.isAvailable("internet") then
 local response = internet.request("http://augitesoul.fr/cgi-bin/fortune4.sh")
 if response then
   for fort in response do
@@ -27,55 +28,42 @@ if response then
       end
      end
   end
+else end
 
 os.sleep(0.25)
 term.write("Loading data...\n\n")
 
 --actually obtaining and showing the data
-os.sleep(0.25)
 gpu.setForeground(0xFFFFFF)
-term.write("Computer address:")
-term.write(computer.address())
-term.write("\n")
+
+os.sleep(0.25)
+term.write("Computer address: "..computer.address().."\n")
 computer.beep(440)
 
 os.sleep(0.25)
-term.write("Current RAM used/free/total:")
 local usedMem = (computer.totalMemory() - computer.freeMemory())
 local usedK = (usedMem/1024)
-term.write(usedK)
-term.write("/")
-term.write(computer.freeMemory()/1024)
-term.write("/")
-term.write(computer.totalMemory()/1024)
-term.write("\n")
+term.write("Current RAM used/free/total: "..usedK .."/"..computer.freeMemory()/1024 .."/"..computer.totalMemory()/1024 .."\n")
 computer.beep(540)
 
 os.sleep(0.25)
-term.write("Energy available/maximum:")
-term.write(computer.energy())
-term.write("/")
-term.write(computer.maxEnergy())
-term.write("\n")
+term.write("Energy available/maximum: "..computer.energy().."/"..computer.maxEnergy().."\n")
 computer.beep(640)
 
 os.sleep(0.25)
-term.write("Uptime in seconds:")
-term.write(computer.uptime())
-term.write("\n")
+term.write("Uptime in seconds: "..computer.uptime().."\n")
 computer.beep(740)
 
 os.sleep(0.25)
-term.write("Booting first from:")
-term.write(computer.getBootAddress())
-term.write("\n")
+term.write("Booting first from: "..computer.getBootAddress().."\n")
 computer.beep(840)
 
 os.sleep(0.25)
 term.write("Can do HTTP requests? ")
-if internet then
-print("true") else
-print("false")
+if component.isAvailable("internet") then
+term.write("true")
+else
+term.write("false")
 end
 term.write("\n")
 computer.beep(940)
@@ -86,6 +74,6 @@ component.list()
 for address, componentType in component.list() 
 do
 os.sleep(0.125) 
-print(address, " ", componentType)
+print(address, componentType)
 computer.beep(340) 
 end
